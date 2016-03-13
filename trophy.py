087cb945066cc0af157ff04ff4f4b00e5e54ea3d
Brian Ly <briancly@berkeley.edu>
2016-03-13 01:28:27 -0800

    Simplify indexing variable and fix to snake_case

--- trophy.py
+++ trophy.py
@@ -7,22 +7,20 @@ platinum = input('Platinum trophies: ')
 
 points = 15 * bronze + 30 * silver + 90 * gold + 180 * platinum
 
-levelArray=[0,200,600,1200,2400,4000]
+level_array=[0,200,600,1200,2400,4000]
 i = 0
-j = 0
-k = 0
 while i < 6:
-	levelArray.append(levelArray[i+5] + 2000)
+	level_array.append(level_array[i+5] + 2000)
 	i += 1
-while j < 14:
-	levelArray.append(levelArray[j+11] + 8000)
-	j += 1
-while points > levelArray[-1]:
-	levelArray.append(levelArray[k+25] + 10000)
-	k += 1
+while i < 20:
+	level_array.append(level_array[i+5] + 8000)
+	i += 1
+while points > level_array[-1]:
+	level_array.append(level_array[i+5] + 10000)
+	i += 1
 level = 1
-while points > levelArray[level-1]:
+while points > level_array[level-1]:
 	level += 1
 else:
-	left = levelArray[level-1] - points
+	left = level_array[level-1] - points
 	print('You are currently at level ' + str(level-1) + '. You need ' + str(left) + ' points until level ' + str(level) + '.')
